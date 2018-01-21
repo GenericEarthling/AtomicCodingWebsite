66881e1805df4f9ab5e795ee2d7f57c052550345
GenericEarthling <sharon.tender@icloud.com>
2018-01-21 16:04:30 -0500

    added php image moved life_ folder to php-prog. folder

---
+++ php-programming/life_happens/config.php
@@ -0,0 +1,18 @@
+<?php
+
+$db_username = 'stender';
+$db_password = 'RaT3oTu-42';
+$db_name = 'LifeHappens';
+$db_host = 'mysql.yaacotu.com'; // 107.180.40.137  // localhost
+
+// can declare global variables here
+
+
+//connect to MySql						
+$mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);						
+if ($mysqli->connect_error) {
+    die('FROM Config.php ERROR: ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
+}
+
+
+?>
